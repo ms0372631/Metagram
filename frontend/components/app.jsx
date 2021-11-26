425ddd206a63d226a3976998b28e6ec73596e7d4
Alan Chiu <ms0372631@gmail.com>
2021-11-26 00:55:04 -0800

    story components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import NavBarContainer from "./nav_bar/nav_bar_container";
 import PostIndexContainer from './posts/post_index_container';
+import StoryIndexContainer from "./stories/story_index_container";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route, Link } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ const App = () => (
   <div>
     <Route path='/' component={NavBarContainer} />
     <Route path='/feed' component={PostIndexContainer}/>
+    <Route path='/feed' component={StoryIndexContainer}/>
     <AuthRoute exact path='/login' component={LoginFormContainer}/>
     <AuthRoute exact path='/signup' component={SignupFormContainer}/>
   </div>
--- frontend/components/stories/story_index_container.jsx
+++ frontend/components/stories/story_index_container.jsx
@@ -1,8 +1,14 @@
 
 import {connect} from 'react-redux';
-import StoryIndex from './'
+import StoryIndex from './story_index'
 
 const mSTP = state => ({
   stories: []
 });
+
+const mDTP = dispatch => ({
+
+});
+
+export default connect(mSTP, mDTP)(StoryIndex);
 

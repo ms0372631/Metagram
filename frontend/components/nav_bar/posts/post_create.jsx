3d04b1bbbcf1ca21e75bba55cc3b690306b6a3ce
Alan Chiu <ms0372631@gmail.com>
2021-11-29 11:39:04 -0800

    modal css

--- frontend/components/nav_bar/posts/post_create.jsx
+++ frontend/components/nav_bar/posts/post_create.jsx
@@ -32,30 +32,25 @@ class PostCreate extends React.Component {
         <div className="modal-screen">
           <div className="modal-form">
             <div className="modal header">
-              <div className="modal header-back"></div>
+              <div className="modal header-back">back</div>
               <div className="modal header-title">Create new post</div>
               <button className="modal header-share"type="submit" form="myform">Share</button>
             </div>
             <div className="modal bottom">
               <div className="modal bottom-left">
-                <p>jkdcjdkc</p>
-                <p>kdcdkcmds,</p>
-                <form className="modal-form" onSubmit={this.handleSubmit}>
+                <form className="modal modal-picture" onSubmit={this.handleSubmit}>
                   <button type="submit">Select from computer</button>
                 </form>
               </div>
               <div className="modal bottom-right">
-                <form id="myform" className="modal-form">
-                  <p>cdkcnmdksc</p>
-                  <p>dkcmdlkscmlksdc</p>
-                  <div className="modal username"/>
-                  <div className="modal text-area"/>
-                  <div className="modal-from"></div>
-                </form>
-                <div className="modal-screen js-modal-close"></div>
+                <div className="modal username"/>
+                <div className="modal text-area"/>
+                <div className="modal form">
+              </div>
               </div>
             </div>
           </div>
+          <div className="modal-screen js-modal-close"></div>
         </div>
       </div>
     ) 

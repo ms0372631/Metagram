ea79a10098f4816f9dec9a9b1e20f245cc131a70
Alan Chiu <ms0372631@gmail.com>
2021-12-13 06:38:47 -0800

    dev prod setup

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Test
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -18,4 +19,3 @@ module Test
   end
 end
 
-config.assets.initialize_on_precompile = false

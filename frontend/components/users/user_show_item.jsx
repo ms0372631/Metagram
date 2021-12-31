4a7e2f98faad34423b3364586b4e6f64f5bd9657
Alan Chiu <ms0372631@gmail.com>
2021-12-31 08:02:20 -0800

    UserShowItem number of likes

--- frontend/components/users/user_show_item.jsx
+++ frontend/components/users/user_show_item.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { requestComments } from '../../actions/comment_actions';
+import { requestPostLikes } from '../../actions/post_like_actions';
 
 
 class UserShowItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = ({
+      numberofLikes: 0
+    })
   }
 
   componentDidMount() {
-    this.props.requestComments(this.props.post.id)
+    this.props.requestComments(this.props.post.id);
+    this.props.requestPostLikes(this.props.post.id)
+    .then(
+      payload => this.setState({
+        numberofLikes: Object.values(payload.postLikes).length
+      })
+    )
   }
 
   render() { 
     
     const {post, comments} = this.props;
-
 
     return (
       <div className="gallery-item" tabIndex="0">
@@ -24,7 +33,7 @@ class UserShowItem extends React.Component {
           <ul>
             <li className="gallery-item-likes">
               <span className="visually-hidden">Likes:</span>
-              <i className="fas fa-heart" aria-hidden="true"></i>
+              <i className="fas fa-heart" aria-hidden="true">{this.state.numberofLikes}</i>
             </li>
             <li className="gallery-item-comments">
               <span className="visually-hidden">Comments:</span>
@@ -38,11 +47,12 @@ class UserShowItem extends React.Component {
 }
 
 const mSTP = (state, ownProps) => ({
-  comments: Object.values(state.entities.comments).filter(comment => comment.postId === ownProps.post.id)
+  comments: Object.values(state.entities.comments).filter(comment => comment.postId === ownProps.post.id),
 })
 
 const mDTP = dispatch => ({
-  requestComments: postId => dispatch(requestComments(postId))
+  requestComments: postId => dispatch(requestComments(postId)),
+  requestPostLikes: postId => dispatch(requestPostLikes(postId))
 })
 
 export default connect(mSTP, mDTP)(UserShowItem);

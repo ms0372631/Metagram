632a0433c603d0293e5486571dd1f026ccb86f07
Alan Chiu <ms0372631@gmail.com>
2022-01-12 11:52:47 -0800

    created following reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import usersReducer from "./users_reducer";
 import postsReducer from "./posts_reducer";
 import commentsReducer from "./comments_reducer";
 import postLikesReducer from "./post_likes_reducer";
+import followingReducer from "./followingReducer";
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   posts: postsReducer,
   comments: commentsReducer,
-  postLikes: postLikesReducer
+  postLikes: postLikesReducer,
+  followings: followingReducer
 });
 
 export default entitiesReducer;

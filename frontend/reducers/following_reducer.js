5880191574ab493fbc60ac7ab7b1903de8ad6bc7
Alan Chiu <ms0372631@gmail.com>
2022-01-12 11:51:07 -0800

    created following reducer

---
+++ frontend/reducers/following_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_FOLLOWING, RECEIVE_FOLLOWINGS, REMOVE_FOLLOWING } from "../actions/following_actions"
+
+const followingReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const nextState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_FOLLOWING:
+      nextState[action.following.id] = aciton.following;
+      return nextState;
+    case RECEIVE_FOLLOWINGS:
+      return Object.assign({}, actions.followings);
+    case REMOVE_FOLLOWING:
+      delete nextState[action.following.id];
+      return nextState;
+    default:
+      return state;
+  }
+}
+
+export default followingReducer;

5fbb14e141b86512217f6681084b75a7afd292be
Alan Chiu <ms0372631@gmail.com>
2022-01-12 10:41:53 -0800

    following api util

---
+++ frontend/util/following_api_util.js
@@ -0,0 +1,29 @@
+
+export const fetchFollowings = userId => (
+  $.ajax({
+    method: 'GET',
+    url: `api/users/${userId}/followings`
+  })
+)
+
+export const fetchFollowing = (userId, followingId) => (
+  $.ajax({
+    method: 'GET',
+    url: `api/users/${userId}/followings/${followingId}`
+  })
+)
+
+export const createFollowing = (following, userId) => (
+  $.ajax({
+    method: 'POST',
+    url: `api/users/${userId}/followings`
+    data: { following }
+  })
+)
+
+export const deleteFollowing = ( userId, followingId ) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `api/users/${userId}/followings/${followingId}`
+  })
+)

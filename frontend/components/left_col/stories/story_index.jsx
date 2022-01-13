687fc3a9bab6691298a2420bbfb6859be5a4f65e
Alan Chiu <ms0372631@gmail.com>
2022-01-13 10:24:44 -0800

    following logic error fixed

--- frontend/components/left_col/stories/story_index.jsx
+++ frontend/components/left_col/stories/story_index.jsx
@@ -14,7 +14,7 @@ class StoryIndex extends React.Component {
 
   render() {
 
-    const {users, followings} = this.props;
+    const {users, followings, currentUser} = this.props;
 
     if (Object.keys(users).length === 0)
       return "";
@@ -22,7 +22,7 @@ class StoryIndex extends React.Component {
     let followingStories = [];
     for (let i = 0; i < users.length; ++i) {
       for (let j = 0; j < followings.length; ++j) {
-        if (users[i].id === followings[j].receiverId)
+        if (users[i].id === followings[j].receiverId && followings[j].authorId === currentUser.id)
           followingStories.push(users[i]);
       }
     }

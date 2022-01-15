c87c4eb8ec8a3ae261bc139d924290a23c9098c2
Alan Chiu <ms0372631@gmail.com>
2022-01-15 05:44:54 -0800

    following logic

--- frontend/components/left_col/stories/story_index.jsx
+++ frontend/components/left_col/stories/story_index.jsx
@@ -22,8 +22,10 @@ class StoryIndex extends React.Component {
     let followingStories = [];
     for (let i = 0; i < users.length; ++i) {
       for (let j = 0; j < followings.length; ++j) {
-        if (users[i].id === followings[j].receiverId && followings[j].authorId === currentUser.id)
+        if (users[i].id === followings[j].receiverId && followings[j].authorId === currentUser.id) {
           followingStories.push(users[i]);
+          break;
+        }
       }
     }
       
